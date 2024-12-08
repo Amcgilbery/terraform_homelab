4d560d89217c341a7be50fea557691efafdf1ad5
Amcgilbery <96699750+Amcgilbery@users.noreply.github.com>
2024-12-08 15:28:16 -0800

    Adding changes to main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,39 @@
-data "aws_region" "current" {}
-data "aws_availability_zone" "available" {}
+data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "app"{
-    cidr_block = "172.16.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    tags = {
-      Name = "app"
-    }
+resource "aws_vpc" "app" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    Name = "app"
+  }
+}
+
+resource "aws_internet_gateway" "app_vpc_igw" {
+  vpc_id = aws_vpc.app.id
 }
 
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.app.id
-    count = 3
-    cidr_block = cidrsubnet(aws_vpc.app.cidr_block, 8, count.index)
-    availability_zone = data.aws_availability_zone.available.names[count.index]
-    tags = {
-      Name = "public-subnet-${count-index}"
-    }
+  vpc_id = aws_vpc.app.id
+  count = 2
+  cidr_block = cidrsubnet(aws_vpc.app.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
 }
 
 resource "aws_subnet" "private" {
-    vpc_id = aws_vpc.app.id
-    count = 3
-    cidr_block = cidrsubnet(aws_vpc.app.cidr_block, 8, count.index)
-    availability_zone = data.aws_availability_zone.available.names[count.index]
-    tags = {
-      Name = "private-subnet-${count-index}"
-    }
+  vpc_id = aws_vpc.app.id
+  count = 2
+  cidr_block = cidrsubnet(aws_vpc.app.cidr_block, 8, count.index + 3)
+
+  # Corrected availability zone reference
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
 }
--- versions.tf
+++ versions.tf
@@ -5,6 +5,12 @@ terraform {
     region = "us-west-1"
     encrypt = true
   }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
